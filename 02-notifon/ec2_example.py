8706ab988d8c94fd3e8e812b450621dceae99b23
ian.hsu@novartis.com <ian.hsu@novartis.com>
2020-05-02 23:53:10 -0400

    First commit for notifon project

---
+++ 02-notifon/ec2_example.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='Ian-pfb')
+ec2 = session.resource('ec2')
+key_name = 'python_automation_key'
+key_path = key_name + '.pem'
+key = ec2.create_key_pair(KeyName=key_name)
+key.key_material
+with open(key_path, 'w') as key_file:
+    key_file.write(key.key_material)
+    
+get_ipython().run_line_magic('ls', '-l python_automation_key.pem')
+import os, stat
+os.chmod(key_path, stat.S_IRUSR | stat.S_IWUSR)
+get_ipython().run_line_magic('ls', '-l python_automation_key.pem')
+ec2.images.filter(Owners=['amazon'])
+list(ec2.images.filter(Owners=['amazon']))
+len(list(ec2.images.filter(Owners=['amazon'])))
+img = ec2.Image('ami-0f7919c33c90f5b58')
+img.name
+ec2_apse2 = session.resource('ec2', region_name='ap-southeast-2')
+img_apse2 = ec2_apse2.Image('ami-0f7919c33c90f5b58')
+img_apse2.name
+img.name
+ami_name = 'amzn2-ami-hvm-2.0.20200406.0-x86_64-gp2'
+filters = [{'Name': 'name', 'Values': [ami_name]}]
+list(ec2.images.filter(Owners=['amazon'], Filters=filters))
+list(ec2_apse2.images.filter(Owners=['amazon'], Filters=filters))
+img
+key
+instances = ec2.create_instances(ImageId=img.id, MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName=key.key_name)
+instances
+ec2.Instance(id='i-0654eda724d94e691')
+inst = instances[0]
+inst.terminate()
+instances = ec2.create_instances(ImageId=img.id, MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName=key.key_name)
+inst= instances[0]
+inst.public_dns_name
+inst.wait_until_running()
+inst.reload()
+inst.public_dns_name
+inst.security_groups
+# Look up the security group
+# Authorize incoming connections from our public IP address, on port 22 (the port SSH uses)
+sg = ec2.SecurityGroup(inst.security_groups[0]['GroupId'])
+sg.authorize_ingress(IpPermissions=[{'FromPort': 22, 'ToPort': 22, 'IpProtocol': 'TCP', 'IpRanges': [{'CidrIp': '204.89.58.1/32'}]}])
+sg.authorize_ingress(IpPermissions=[{'FromPort': 80, 'ToPort': 80, 'IpProtocol': 'TCP', 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}])
+inst.public_dns_name
